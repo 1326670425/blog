5236baba8bfb86b92788cadf8b3b1b4561dc859d
1326670425 <1326670425@qq.com>
2020-09-01 11:49:52 +0800

    add

---
+++ src/main/java/cn/novalue/blog/model/constants/RedisConstant.java
@@ -0,0 +1,12 @@
+package cn.novalue.blog.model.constants;
+
+/**
+ * Redis用到的一些常量
+ *
+ * @author Wu Yangjie
+ * @date 2020-08-30
+ */
+public class RedisConstant {
+    public static final String FRIEND = "friend:";
+    public static final String NOTIFY = "notify:";
+}
