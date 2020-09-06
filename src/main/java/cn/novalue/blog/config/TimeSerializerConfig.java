6ac705c127eaa1c80760e9966d18057698c81e52
1326670425 <1326670425@qq.com>
2020-09-06 23:43:38 +0800

    添加LocalDate的序列化支持

--- src/main/java/cn/novalue/blog/config/TimeSerializerConfig.java
+++ src/main/java/cn/novalue/blog/config/TimeSerializerConfig.java
@@ -1,11 +1,13 @@
 package cn.novalue.blog.config;
 
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
@@ -20,18 +22,23 @@ import java.util.Locale;
 public class TimeSerializerConfig {
 
     @Value("${spring.jackson.date-format:yyyy-MM-dd HH:mm:ss}")
+    private String dateTimeFormat;
+    @Value("${spring.jackson.date-format:yyyy-MM-dd}")
     private String dateFormat;
     @Value("${spring.jackson.local:zh_CN}")
     private Locale local;
 
 
     private LocalDateTimeSerializer localDateTimeDeserializer() {
-        return new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(dateFormat, local));
+        return new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(dateTimeFormat, local));
     }
-
+    private LocalDateSerializer localDateDeserializer() {
+        return new LocalDateSerializer(DateTimeFormatter.ofPattern(dateFormat, local));
+    }
+    
     @Bean
     public Jackson2ObjectMapperBuilderCustomizer jackson2ObjectMapperBuilderCustomizer() {
-        return builder -> builder.serializerByType(LocalDateTime.class, localDateTimeDeserializer());
-    }
+        return builder -> builder.serializerByType(LocalDateTime.class, localDateTimeDeserializer())
+                .serializerByType(LocalDate.class, localDateDeserializer());    }
 
 }
